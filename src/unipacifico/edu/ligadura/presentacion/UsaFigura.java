889afaa9788e5ce5e6de56ede8038d43f1e0ec40
ALUMNO- <ALUMNO-@ALUMNO-22>
2024-04-30 08:19:15 -0500

    inicializando el proyecto

---
+++ src/unipacifico/edu/ligadura/presentacion/UsaFigura.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unipacifico.edu.ligadura.presentacion;
+
+/**
+ *
+ * @author ALUMNO-
+ */
+public class UsaFigura {
+    public static void main(String[] args) {
+        
+        Rectangulo rect = new Rectangulo();
+    //    System.out.println(rect.dibujar());
+        
+        
+        Circulo cir = new LienzoFigura();
+        Triangulo tri = new Triangulo();
+        
+        LienzoFigura lienzo = new LienzoFigura();
+        System.out.println(lienzo.dibujarFigura(iri));
+        
+    }
+    
+}
